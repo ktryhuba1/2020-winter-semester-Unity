72af1c9fd60ebe8aaa6b5fcd8d9e7018307a716b
ktryhuba1 <46577134+ktryhuba1@users.noreply.github.com>
2020-02-10 09:49:26 -0700

    bits

---
+++ sampleScenes/Assets/CounterDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterDisplay : MonoBehaviour
+{
+    GameState theGameState;
+    int score;
+    private void Awake()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("GameState");
+        theGameState = gameObjects[0].GetComponent<GameState>();
+
+        score = theGameState.counter;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponent<Text>().text = this.gameObject.GetComponent<Text>().text + score; 
+    }
+}
---
+++ sampleScenes/Assets/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+
+    public int counter = 0;
+    private void Awake()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("GameState");
+
+        if(gameObjects.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(counter++);
+    }
+
+}
