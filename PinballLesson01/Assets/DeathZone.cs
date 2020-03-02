a17443f2eebd46a61137850a48a8e30dda872d24
ktryhuba1 <46577134+ktryhuba1@users.noreply.github.com>
2020-03-02 09:26:12 -0700

    started pinball

---
+++ PinballLesson01/Assets/DeathZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    public Transform ballspawnPoint;
+    public GameObject ballPrefab;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Destroy(collider.gameObject);
+        StartCoroutine(Wait(collider));     
+    }
+
+    IEnumerator Wait(Collider2D collider)
+    {
+        yield return new WaitForSeconds(1);
+
+        collider.transform.position = ballspawnPoint.position;
+        collider.attachedRigidbody.velocity = Vector2.zero;
+
+    }
+}
+
+
---
+++ PinballLesson01/Assets/ballspawnPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballspawnPoint : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 0.1f);
+    }
+}
