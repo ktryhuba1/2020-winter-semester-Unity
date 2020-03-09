442496b4d3f7c0f956351fbc82f33e011d7699b0
ktryhuba1 <46577134+ktryhuba1@users.noreply.github.com>
2020-03-09 09:19:07 -0600

    +1
    changes

---
+++ PinballLesson01/Assets/bumper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bumper : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        Rigidbody2D rb2d = collider.gameObject.GetComponent<Rigidbody2D>();
+        rb2d.AddForce(6 * collider.relativeVelocity);
+    }
+}
